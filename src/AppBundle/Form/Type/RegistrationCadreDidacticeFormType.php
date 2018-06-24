535cbfaf5ffe9480bb62be5aa55e5bec0e2ea7eb
Mihai Iorga <mihai.iorga@bearingpoint.com>
2018-06-24 17:02:46 +0300

    registration

--- src/AppBundle/Form/Type/RegistrationCadreDidacticeFormType.php
+++ src/AppBundle/Form/Type/RegistrationCadreDidacticeFormType.php
@@ -3,13 +3,18 @@
 namespace AppBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
 
 class RegistrationCadreDidacticeFormType extends AbstractType {
 
   public function buildForm(FormBuilderInterface $builder, array $options) {
-    parent::buildForm($builder, $options);
+    $builder
+      ->add('nume', TextType::class, $options = ['required' => TRUE])
+      ->add('prenume', TextType::class, $options = ['required' => TRUE])
+      ->add('grad', TextType::class, $options = ['required' => TRUE])
+      ->remove('username');
   }
 
   public function getParent() {
